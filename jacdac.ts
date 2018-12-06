197070eebceae26220788edae2360689cacf10be
Teddy Seyed <t-tesey@microsoft.com>
2018-12-06 11:31:52 -0800

    Updates.

--- jacdac.ts
+++ jacdac.ts
@@ -1,6 +1,5 @@
 // Add your code here
 namespace jacdac {
-
     export class DRV2605L extends ActuatorHostDriver {
         constructor(name: string) {
             super(name, 300, 5);
@@ -11,12 +10,11 @@ namespace jacdac {
             let duration = this.state.getNumber(NumberFormat.UInt32LE, 1);
 
             if (duration > 0) {
-
+                this.log(`playing ${effect} for ${duration}ms`)
+                drv2605.playEffect(effect, duration);
             }
 
             return true;
         }
     }
-
-
 }
